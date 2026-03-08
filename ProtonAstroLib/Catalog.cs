1bce9c8988ca2da539d972a1f6775b1aa409d5f6
Koen van Leeuwen <koen@logiqs.nl>
2026-03-08 14:15:41 +0100

    Add some southern hemisphere entries

--- ProtonAstroLib/Catalog.cs
+++ ProtonAstroLib/Catalog.cs
@@ -11,12 +11,23 @@ namespace ProtonAstroLib
         public static readonly EquatorialCoordinate Polaris = EquatorialCoordinate.FromRaDec("02:31:49.1", "89d15m51s");
         public static readonly EquatorialCoordinate Sirius = EquatorialCoordinate.FromRaDec("06:45:08.9", "-16d42m58s");
         public static readonly EquatorialCoordinate Vega = EquatorialCoordinate.FromRaDec("18:36:56.3", "38d47m01s");
+        public static readonly EquatorialCoordinate Canopus = EquatorialCoordinate.FromRaDec("06:23:57.1", "-52d41m44s");
+        public static readonly EquatorialCoordinate Achernar = EquatorialCoordinate.FromRaDec("01:37:42.8", "-57d14m12s");
+        public static readonly EquatorialCoordinate AlphaCentauri = EquatorialCoordinate.FromRaDec("14:39:36.5", "-60d50m02s");
+        public static readonly EquatorialCoordinate Acrux = EquatorialCoordinate.FromRaDec("12:26:35.9", "-63d05m57s");
 
         // Messier objects
         public static readonly EquatorialCoordinate CrabNebula = EquatorialCoordinate.FromRaDec("05:34:32", "22d00m52s");
         public static readonly EquatorialCoordinate AndromedaGalaxy = EquatorialCoordinate.FromRaDec("00:42:44", "41d16m09s");
         public static readonly EquatorialCoordinate OrionNebula = EquatorialCoordinate.FromRaDec("05:35:17", "-05d23m28s");
         public static readonly EquatorialCoordinate Pleiades = EquatorialCoordinate.FromRaDec("03:47:00", "24d07m00s");
+
+        // Southern DSOs
+        public static readonly EquatorialCoordinate LargeMagellanicCloud = EquatorialCoordinate.FromRaDec("05:23:34", "-69d45m22s");
+        public static readonly EquatorialCoordinate SmallMagellanicCloud = EquatorialCoordinate.FromRaDec("00:52:45", "-72d49m43s");
+        public static readonly EquatorialCoordinate OmegaCentauri = EquatorialCoordinate.FromRaDec("13:26:47", "-47d28m46s");
+        public static readonly EquatorialCoordinate EtaCarinaeNebula = EquatorialCoordinate.FromRaDec("10:43:50", "-59d52m04s");
+        public static readonly EquatorialCoordinate CentaurusA = EquatorialCoordinate.FromRaDec("13:25:28", "-43d01m09s");
 
         /// <summary>
         /// Computes the Sun's equatorial coordinates (equinox-of-date) for a given moment.
--- TelescopeDrive/Models/CatalogEntries.cs
+++ TelescopeDrive/Models/CatalogEntries.cs
@@ -14,10 +14,19 @@ public static class CatalogEntries
         new("Betelgeuse", Catalog.Betelgeuse),
         new("Rigel", Catalog.Rigel),
         new("Aldebaran", Catalog.Aldebaran),
+        new("Canopus", Catalog.Canopus),
+        new("Achernar", Catalog.Achernar),
+        new("Alpha Centauri", Catalog.AlphaCentauri),
+        new("Acrux", Catalog.Acrux),
         new("M1 - Crab Nebula", Catalog.CrabNebula),
         new("M31 - Andromeda", Catalog.AndromedaGalaxy),
         new("M42 - Orion Nebula", Catalog.OrionNebula),
         new("M45 - Pleiades", Catalog.Pleiades),
+        new("LMC - Large Magellanic Cloud", Catalog.LargeMagellanicCloud),
+        new("SMC - Small Magellanic Cloud", Catalog.SmallMagellanicCloud),
+        new("NGC 5139 - Omega Centauri", Catalog.OmegaCentauri),
+        new("NGC 3372 - Eta Carinae Nebula", Catalog.EtaCarinaeNebula),
+        new("NGC 5128 - Centaurus A", Catalog.CentaurusA),
     ];
 
     public static CatalogEntry? FindByName(string name) =>
